6e897ac8b26aee8a7f0312d5d340012ee1f40a14
Rahul Gowda <160992562+gowdarahul642@users.noreply.github.com>
2026-07-12 20:17:25 +0530

    Time: 1 ms (5%), Space: 19.2 MB (88.05%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,6 +2,6 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         sum=""
         for i in digits:
-            sum+=str(i)
-        sum=str(int(sum)+1)
-        return [int(i) for i in sum]
+            sum=sum+str(i)
+        sum=int(sum)+1
+        return [int(i) for i in str(sum)]
