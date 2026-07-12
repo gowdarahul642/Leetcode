646e363cb68da639d00662b9784a6aa2fa4744da
Rahul Gowda <160992562+gowdarahul642@users.noreply.github.com>
2026-07-12 14:14:02 +0530

    Time: 0 ms (100%), Space: 19.4 MB (7.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -6,8 +6,9 @@ class Solution:
             if s[i]==t[j]:
                 i+=1
             j+=1
-        if i==len(s):
-            return True
-        return False
+        return len(s)==i
+
+
+   
        
         
