e6d483acaf181a4c741c59fa34b71fe41f599a86
Rahul Gowda <160992562+gowdarahul642@users.noreply.github.com>
2026-07-22 21:32:13 +0530

    Time: 7 ms (40.56%), Space: 20.7 MB (23.75%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,5 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
+        '''
         z=[]
         for i in range (len(nums)):
             if nums[i]!=0:
@@ -8,7 +9,13 @@ class Solution:
                 z.append(0)
                 
         for i in range(len(z)):
-            nums[i]=z[i]
+            nums[i]=z[i]'''
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+
 
 
                 
